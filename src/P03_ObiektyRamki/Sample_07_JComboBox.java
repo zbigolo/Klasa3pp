9a541fca261556d54e3f444b02515b62536b7553
zbigolo <zbigolo@gazeta.pl>
2023-01-02 14:51:04 +0100

    zmiany

--- src/P03_ObiektyRamki/Sample_07_JComboBox.java
+++ src/P03_ObiektyRamki/Sample_07_JComboBox.java
@@ -1,6 +1,7 @@
 package P03_ObiektyRamki;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
@@ -10,31 +11,36 @@ public class Sample_07_JComboBox {
 
     private JComboBox wyborOS;
     private JLabel ikona;
-    private JLabel tekst, tempselected,tempdeselected;
+    private JLabel tekst, tempselected, tempdeselected;
+    private JPanel gora, dol;
 
 
-    private JPanel myContentPane(){
+    private JPanel myContentPane () {
         JPanel mypanel = new JPanel(null);
-        String[] systemy = {"","Windows", "Linux", "MacOS","DOS", "Unix"};
+        String[] systemy = {"", "Windows", "Linux", "MacOS", "DOS", "Unix"};
+
+        gora = new JPanel(null);
+        gora.setBounds(0, 0, 400, 90);
+        gora.setBackground(Color.green);
 
         wyborOS = new JComboBox<>(systemy);
-        wyborOS.setBounds(50,40,200,25);
+        wyborOS.setBounds(50, 40, 200, 25);
 
 
         ikona = new JLabel();
-        ikona.setBounds(100,100,64,64);
+        ikona.setBounds(100, 100, 64, 64);
         ikona.setVisible(true);
 
         tekst = new JLabel();
-        tekst.setBounds(200,125,200,30);
+        tekst.setBounds(200, 125, 200, 30);
         tekst.setVisible(true);
 
         tempselected = new JLabel();
-        tempselected.setBounds(150,200,200,30);
+        tempselected.setBounds(150, 200, 200, 30);
         tempselected.setVisible(true);
 
         tempdeselected = new JLabel("Wybierz system operacyjny ...");
-        tempdeselected.setBounds(50, 15,200,30);
+        tempdeselected.setBounds(50, 15, 200, 30);
         tempdeselected.setVisible(true);
 
         wyborOS.addActionListener(new ActionListener() {
@@ -44,7 +50,7 @@ public class Sample_07_JComboBox {
 
                 int wybor = wyborOS.getSelectedIndex();
                 System.out.println(wybor);
-                switch (wybor){
+                switch (wybor) {
                     case 1 -> {
                         path = "./grafika/os/win.png";
                         tekst.setText("Twój wybór : Windows");
@@ -57,7 +63,7 @@ public class Sample_07_JComboBox {
                         path = "./grafika/os/mac.png";
                         tekst.setText("Twój wybór : MacOS");
                     }
-                    case 4 ->{
+                    case 4 -> {
                         path = "./grafika/os/dos.png";
                         tekst.setText("Twój wybór : DOS OS");
                     }
@@ -73,23 +79,30 @@ public class Sample_07_JComboBox {
                 String tmp = "Wybrałeś: ";
                 String tmp1 = "";
 
-                if (state == ItemEvent.SELECTED){
+                if (state == ItemEvent.SELECTED) {
                     tmp += systemy[wyborOS.getSelectedIndex()];
                     if (wyborOS.getSelectedIndex() > 0)
-                    tempselected.setText(tmp);
+                        tempselected.setText(tmp);
                 }
-                if (state == ItemEvent.DESELECTED){
+                if (state == ItemEvent.DESELECTED) {
                     System.out.println("DESELECTED : " + e.getItem());
                 }
 
             }
         });
-        mypanel.add(ikona); mypanel.add(tekst);
-        mypanel.add(tempselected); mypanel.add(tempdeselected);
-        mypanel.add(wyborOS);
+
+
+        mypanel.add(ikona);
+        mypanel.add(tekst);
+        mypanel.add(tempselected);
+        mypanel.add(tempdeselected);
+        mypanel.add(gora);
+        //l.add(dol);
+        gora.add(wyborOS);
         return mypanel;
     }
-    public Sample_07_JComboBox(){
+
+    public Sample_07_JComboBox () {
         JFrame myWindow = new JFrame("Sample JComboBox");
         myWindow.setContentPane(myContentPane());
         myWindow.setSize(400, 300);
